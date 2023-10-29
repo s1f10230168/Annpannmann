b70e77e1e82942d71c731413d8288d697e318566
Ai Ebihara <s1f102301684@iniad.org>
2023-10-29 22:44:16 +0900

    あんぱんまん

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,55 +3,29 @@ import styles from './index.module.css';
 const Home = () => {
   return (
     <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <div className={styles.basepoint}>
+        <div className={styles.face} />
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code} style={{ backgroundColor: '#fafafa' }}>
-            pages/index.js
-          </code>
-        </p>
+        <div className={styles.mayuL} />
+        <div className={styles.mayuR} />
+        <div className={styles.mayub1} />
+        <div className={styles.mayub2} />
+        <div className={styles.mouth} />
+        <div className={styles.mouthb} />
+        <div className={styles.hanaL} />
 
-        <div className={styles.grid}>
-          <a className={styles.card} href="https://nextjs.org/docs">
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a className={styles.card} href="https://nextjs.org/learn">
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a className={styles.card} href="https://github.com/vercel/next.js/tree/master/examples">
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            className={styles.card}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <img src="vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+        <div className={styles.hanaR} />
+        <div className={styles.hanab1} />
+        <div className={styles.hanab2} />
+        <div className={styles.hanaL2} />
+        <div className={styles.hanaR2} />
+        <div className={styles.hana} />
+        <div className={styles.hanateriL} />
+        <div className={styles.hanateriR} />
+        <div className={styles.hanateriC} />
+        <div className={styles.eyeR} />
+        <div className={styles.eyeL} />
+      </div>
     </div>
   );
 };
